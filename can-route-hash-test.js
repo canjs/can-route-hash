04d87fc0de16d930113b6e589ea910b7c1966e9d
Justin Meyer <justinbmeyer@gmail.com>
2018-07-03 14:41:36 -0500

    a slightly improved test and docs

--- can-route-hash-test.js
+++ can-route-hash-test.js
@@ -1,4 +1,3 @@
-var canReflect = require('can-reflect');
 var QUnit = require('steal-qunit');
 var helpers = require("./test/helpers");
 
@@ -18,12 +17,50 @@ QUnit.test('Basics', function(){
 });
 
 QUnit.test('Event handlers are called', function(){
-	var teardown = helpers.setup(function(RouteHash){
+	var teardown = helpers.setup(function(RouteHash, canReflect){
 		var hash = new RouteHash();
 		canReflect.onValue(hash, function(){
 			QUnit.ok(true, "it worked");
 			teardown();
 		});
 		hash.value = "foo/bar";
+	});
+});
+
+QUnit.test("test both sides of live binding", function(){
+
+
+	var teardown = helpers.setup(function(RouteHash, canReflect, win){
+
+		// routeHash is initialized to window's hash
+		win.location.hash = "#!foo";
+		var routeHash = new RouteHash();
+
+		QUnit.equal(routeHash.value, "foo");
+
+
+		var next;
+		// Setup a binding
+		var bindingChanges = [];
+		canReflect.onValue( routeHash, function(newVal){
+			bindingChanges.push(newVal);
+			next();
+		});
+
+		next = function(){
+			next = function(){
+				QUnit.deepEqual(bindingChanges,["bar","zed"],"dispatched events");
+				teardown();
+			}
+			// Updating the observable changes the hash
+			routeHash.value = "zed";
+			QUnit.equal(win.location.hash, "#!zed");
+		}
+
+
+		// Updating the hash changes the observable
+		win.location.hash = "#!bar";
+		QUnit.equal(routeHash.value, "bar")
+
 	});
 });
--- can-route-hash.md
+++ can-route-hash.md
@@ -16,15 +16,16 @@ var routeHash = new RouteHash();
 
 window.location.hash = "#!foo";
 
-// bind
+// You must listen on the routeHash before it
+// begins listening for hashchanges
 Reflect.onValue( routeHash, function(){});
 
+// changing the hash updates the observable value
 window.location.hash = "#!bar";
-
 routeHash.value //-> "bar"
 
+// changing the observable value updates the hash
 routeHash.value = "zed";
-
 window.location.hash = "#!zed";
 ```
 
@@ -37,5 +38,23 @@ instances of `RouteHash` implement the common  `ValueObservable` symbols:
 - [can-reflect/observe.offValue] - `canReflect.offValue( routeHash, handler )`
 
 
-
 @body
+
+
+## Use
+
+As `can-route-hash` is the default routing observable used by [can-route], it's typically not
+used directly. Instead it's often replaced by [can-route-mock] for testing, or replaces
+[can-route-pushstate] when hashchange based routing would be preferable.
+
+If [can-route-pushstate] is being used, but it would be useful to switch to hashchange-based routing, you can
+typically map the `can-route-pushstate` package to `can-route-hash`. This is often done with StealJS as follows:
+
+```js
+<script>
+steal = {
+    map: {"can-route-pushstate": "can-route-hash"}
+}
+</script>
+<script src="./node_modules/steal/steal.js" main></script>
+```
